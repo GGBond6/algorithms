4bf73bda3eb5291cf9a9cf79308090648a4a7584
ggbond <ggbond@666.com>
2021-12-15 21:36:27 +0800

    半数集（dfs）

--- Exercise6/7_6_Half_of_the_set.cpp
+++ Exercise6/7_6_Half_of_the_set.cpp
@@ -1,18 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-//对由n的单数集构成的树做深度优先搜索
-int dfs(int n){
-  //至少为本身
-  int res=1;
-  if(n>1){
-    for (int i = 1; i <= n/2; ++i) {
-      res+= dfs(i);
-    }
+//求n的半数集个数
+int dfs(int n) {
+  //递归出口：半数集只有自身
+  if (n <= 1) {
+    return 1;
+  }
+  int res = 1;//自己本身肯定在半数集中，那么res初值为1
+  for (int i = 1; i <= n / 2; ++i) {//可以将半数集看成一个多叉树，这里是递归遍历多叉树
+    res += dfs(i);
   }
   return res;
 }
 
-int main(){
+int main() {
   int n;
   cin >> n;
   cout << dfs(n);
